80383109ccc08ed1fcbb788ce312b7dc05e5829c
sumit <sumitpatil90990@gmail.com>
2021-02-25 12:05:09 +0530

    calculator code

--- calculator.js
+++ calculator.js
@@ -1,10 +1,20 @@
 
-var express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var app = express();
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res)=> {
-  res.send("hello");
+  res.sendFile(__dirname  + "/index.html");
+});
+
+app.post("/", function (req, res){
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var result = num1 + num2;
+
+  res.send("result is " + result);
 });
 
 app.listen(3000, function(){
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Calculator WebApp
+This repo contain code of simple Addition Calculator.
+
+## Description
+This Repo is for learning working of Nodejs and Express.
